62a649e7f9e5c90e15640892df7d70472116257f
Rogan Murley <rogan@potatolondon.com>
2016-08-14 15:16:38 +0100

    Added table wrapper to body

--- src/Translate.hs
+++ src/Translate.hs
@@ -6,6 +6,7 @@ import Control.Monad
 
 import Text.Blaze.Html4.Transitional as H
 import Text.Blaze.Html4.Transitional.Attributes as A
+import Text.Blaze.Internal (attribute, Attribute, AttributeValue)
 
 import Tokens
 
@@ -18,8 +19,11 @@ template b = docTypeHtml $ do
         meta ! httpEquiv "X-UA-Compatible" ! content "IE=edge"                             -- Enable media queries for windows phone 8.
         meta ! name "format-detection"     ! content "telephone=no"                        -- Disable auto telephone linking in iOS.
         H.title "Made with Dreamail"
-    body $ do
-        b
+    body ! bgcolor "#ffffff" ! leftmargin "0" ! topmargin "0" ! marginwidth "0" ! marginheight "0" $ do
+        table ! width "600" ! border "0" ! cellpadding "0" ! cellspacing "0" ! align "center" $
+            tr $
+                td $
+                    b
 
 translate :: [Token] -> Html
 translate xs = template $ forM_ xs translateEach
@@ -28,3 +32,16 @@ translateEach :: Token -> Html
 translateEach (Text x)  = string x
 translateEach (Img s a) = img ! src (toValue s) ! alt (toValue a)
 translateEach (Div c xs)  = H.div ! class_ (toValue c) $ forM_ xs translateEach
+
+
+leftmargin :: AttributeValue -> Attribute
+leftmargin = attribute "leftmargin" " leftmargin=\""
+
+topmargin :: AttributeValue -> Attribute
+topmargin = attribute "topmargin" " topmargin=\""
+
+marginwidth :: AttributeValue -> Attribute
+marginwidth = attribute "marginwidth" " marginwidth=\""
+
+marginheight :: AttributeValue -> Attribute
+marginheight = attribute "marginheight" " marginheight=\""
